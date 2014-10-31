ee5fe4806bb70e83ff20a160a8afa623728d38d3
Skye Griffin <skybox@trioptimum.com>
2014-10-30 21:10:32 -0500

    Trying out few things as I learn to develop the API.
    Implementing the GET /list endpoint that retrieves all rows in the
    database.
    Adding a POST /addItem endpoint that inserts items in to the database
    if they are of a item=<value> form.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var db = new sqlite3.Database( 'data/groceree_db' );
 var port = 8000
 var url = require( 'url' );
 var http = require( 'http' );
+var qString = require( 'querystring' );
 http.createServer( function ( req, res ) {
 
     // Parse out the requested path
@@ -11,27 +12,60 @@ http.createServer( function ( req, res ) {
     var urlParts = req.url.split( "/" );
     var action = urlParts[ 1 ];
     res.writeHead( 200, { 'Content-Type': 'application/json' } );
-    var result;
 
     switch( req.method ) {
         case "GET":
-            if( action == 'list' )
+            if( action == 'list' ) {
                 //console.log( JSON.stringify( req.headers ) );
 
                 // Get rows from our database
                 var stmt = "SELECT rowid AS id, item FROM groceree";
                 db.all( stmt, function( err, rows ) {
-                    //console.log( rows );
+                    console.log( rows );
 
                     res.end( JSON.stringify( rows ) );
                 } );
-            break;
+
+                break;
+            }
         case "POST":
-            if( action == 'list' )
-                console.log( req.headers );
+            if( action == 'addItem' ) {
+                var body = "";
+
+                req.on( 'data', function( chunk ) {
+                    body += chunk
+                } );
+
+                req.on( 'end', function() {
+                    // Parse POST data from request body
+                    var postData = qString.parse( body );
+                    //console.log( postData );
+
+                    // Prepare the db statement
+                    var stmt = db.prepare( "INSERT INTO groceree VALUES (?)" );
+
+                    // Insert POST payload in to the db
+                    // Only works for item=<value> pairs
+                    for( var i = 0; i < postData.item.length; i++ ) {
+                        stmt.run( postData.item[ i ] );
+                    }
+
+                    stmt.finalize();
+
+                    res.writeHead( 200 );
+                    res.end();
+                } );
+
+                break;
+            }
+
+            // If none of the above conditions are met, we will end up here.
+            res.writeHead( 404, { 'Content-Type': 'text/plain' } );
+            res.end( "Endpoint not found" );
             break;
         default:
-            console.log( "unknown" );
+            res.writeHead( 501, { 'Content-Type': 'text/plain' } );
+            res.end( "Request not understood" );
     }
     
     //console.log( result );
